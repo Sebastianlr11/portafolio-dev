5e6809fd4b3cf62e72a02ddecfec972a87f5e0c3
Sebastianlr11 <sebastianlr051@gmail.com>
2025-02-21 10:33:47 -0500

    mejora version 1.4

---
+++ src/data/technologies.js
@@ -0,0 +1,11 @@
+// src/data/technologies.js
+export const technologies = [
+    { src: "/html5.svg", alt: "HTML5", label: "HTML5" },
+    { src: "/css3.svg", alt: "CSS3", label: "CSS3" },
+    { src: "/javascript.svg", alt: "JavaScript", label: "JavaScript" },
+    { src: "/astro.svg", alt: "Astro", label: "Astro" },
+    { src: "/tailwindcss.svg", alt: "Tailwind CSS", label: "Tailwind CSS" },
+    { src: "/git.svg", alt: "Git", label: "Git" },
+    { src: "/github.svg", alt: "GitHub", label: "GitHub" },
+    { src: "/typescript.svg", alt: "TypeScript", label: "TypeScript" },
+  ];
