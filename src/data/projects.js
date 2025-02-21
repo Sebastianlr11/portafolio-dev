a9cb238a8e52de3b2a0f45c7019b00d70af18e0c
Sebastianlr11 <sebastianlr051@gmail.com>
2025-02-21 10:57:28 -0500

    mejora version 1.5

---
+++ src/data/projects.js
@@ -0,0 +1,59 @@
+// src/data/projects.js
+export const projects = [
+    {
+      src: "/proyecto1.png",
+      alt: "Empresa De Prendas Hospitalarias",
+      title: "Empresa De Prendas Hospitalarias",
+      description: "Formulario de registro a un newsletter, acompañado de un mensaje de agradecimiento.",
+      technologies: ["/astro.svg", "/tailwindcss.svg", "/javascript.svg"],
+      github: "https://github.com/Sebastianlr11/mundial-quirurgicos",
+      demo: "https://lucky-banoffee-bf14e9.netlify.app/"
+    },
+    {
+      src: "/proyecto1.png",
+      alt: "Proyecto 2",
+      title: "Proyecto 2",
+      description: "Breve descripción del Proyecto 2.",
+      technologies: ["/astro.svg", "/tailwindcss.svg", "/javascript.svg"],
+      github: "https://github.com/tu-usuario/proyecto2",
+      demo: "https://tu-proyecto2-demo.com"
+    },
+    {
+      src: "/proyecto1.png",
+      alt: "Proyecto 3",
+      title: "Proyecto 3",
+      description: "Breve descripción del Proyecto 3.",
+      technologies: ["/astro.svg", "/tailwindcss.svg", "/javascript.svg"],
+      github: "https://github.com/tu-usuario/proyecto3",
+      demo: "https://tu-proyecto3-demo.com"
+    },
+    {
+      src: "/proyecto1.png",
+      alt: "Proyecto 4",
+      title: "Proyecto 4",
+      description: "Breve descripción del Proyecto 4.",
+      technologies: ["/astro.svg", "/tailwindcss.svg", "/javascript.svg"],
+      github: "https://github.com/tu-usuario/proyecto4",
+      demo: "https://tu-proyecto4-demo.com"
+    },
+    {
+      src: "/proyecto1.png",
+      alt: "Proyecto 5",
+      title: "Proyecto 5",
+      description: "Breve descripción del Proyecto 5.",
+      technologies: ["/astro.svg", "/tailwindcss.svg", "/javascript.svg"],
+      github: "https://github.com/tu-usuario/proyecto5",
+      demo: "https://tu-proyecto5-demo.com",
+      extra: true
+    },
+    {
+      src: "/proyecto1.png",
+      alt: "Proyecto 6",
+      title: "Proyecto 6",
+      description: "Breve descripción del Proyecto 6.",
+      technologies: ["/astro.svg", "/tailwindcss.svg", "/javascript.svg"],
+      github: "https://github.com/tu-usuario/proyecto6",
+      demo: "https://tu-proyecto6-demo.com",
+      extra: true
+    }
+  ];
