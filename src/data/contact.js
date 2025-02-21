06227be4186dec72d73101cf79ba434f56fd1d28
Sebastianlr11 <sebastianlr051@gmail.com>
2025-02-21 11:32:14 -0500

    mejora version 1.8

---
+++ src/data/contact.js
@@ -0,0 +1,25 @@
+// src/data/contact.js
+export const contact = {
+    title: "¿Hablemos?",
+    description: "Si tienes alguna pregunta, propuesta o simplemente quieres saludar, ¡no dudes en contactarme!",
+    links: [
+      {
+        href: "https://www.linkedin.com/in/sebastian-lopez-9969151b0/",
+        icon: "linkedin.svg",
+        label: "LinkedIn",
+        ariaLabel: "LinkedIn"
+      },
+      {
+        href: "https://github.com/Sebastianlr11",
+        icon: "github.svg",
+        label: "GitHub",
+        ariaLabel: "GitHub"
+      },
+      {
+        href: "mailto:sebastianlr051@gmail.com",
+        icon: "mail.svg",
+        label: "sebastianlr051@gmail.com",
+        ariaLabel: "Correo"
+      }
+    ]
+  };
