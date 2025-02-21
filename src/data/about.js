1f69b803bfe28bca2ac688dfe76a1cd8ca8634fa
Sebastianlr11 <sebastianlr051@gmail.com>
2025-02-21 11:28:53 -0500

    mejora version1.7

---
+++ src/data/about.js
@@ -0,0 +1,11 @@
+// src/data/about.js
+export const about = {
+    image: "/image-hero1.png",
+    alt: "Mi Avatar",
+    paragraphs: [
+      "Soy un apasionado profesional del desarrollo web con experiencia en proyectos innovadores y un enfoque en diseño moderno. Me encanta aprender y aplicar nuevas tecnologías para crear soluciones digitales de alto impacto.",
+      "A lo largo de mi carrera, he trabajado en diversos proyectos que me han permitido crecer profesionalmente y ampliar mis conocimientos en áreas como Frontend, Backend y experiencia de usuario. Mi objetivo es seguir innovando y aportar soluciones creativas."
+    ],
+    cvLink: "/resume.pdf",
+    cvLabel: "Descargar CV"
+  };
