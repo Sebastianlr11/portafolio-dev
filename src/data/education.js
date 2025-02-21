be8a08309768c105bae8e055d4862203f1b659b1
Sebastianlr11 <sebastianlr051@gmail.com>
2025-02-21 11:16:29 -0500

    mejora version 1.6

---
+++ src/data/education.js
@@ -0,0 +1,24 @@
+// src/data/education.js
+export const education = [
+    {
+      title: "Licenciatura en Ingeniería de Software",
+      date: "2023",
+      certification: "Certificado",
+      institution: "Universidad X, Ciudad, País",
+      description: "Breve descripción de la carrera."
+    },
+    {
+      title: "Curso de Desarrollo Web Full Stack",
+      date: "2021",
+      certification: "Autodidacta",
+      institution: "Academia Y, Ciudad, País",
+      description: "Breve descripción del curso."
+    },
+    {
+      title: "Certificación en UX/UI Design",
+      date: "2020",
+      certification: "Certificado",
+      institution: "Instituto Z, Ciudad, País",
+      description: "Breve descripción del certificado."
+    }
+  ];
